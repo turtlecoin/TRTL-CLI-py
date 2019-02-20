65c2330173224cdf5caf3463fead6c66e2b89d34
Sajo8 <33220209+Sajo8@users.noreply.github.com>
2019-02-20 12:56:39 -0700

    better code for nodes

--- nodes.py
+++ nodes.py
@@ -26,18 +26,22 @@ def nodes():
     t = PrettyTable(['Name', 'URL', 'Port', 'SSL'])
 
     nodes = requests.get('https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json').json()
+    nodes = nodes['nodes']
 
-    for x in nodes['nodes']:
-        node_name = Fore.GREEN + x['name'] + Fore.RESET
-        url_link = Fore.GREEN + x['url'] + Fore.RESET
-        port_no = Fore.YELLOW + str(x['port']) + Fore.RESET
-        if x['ssl']:
-            ssl_status = Fore.GREEN + str(x['ssl']) + Fore.RESET
-        else:
-            ssl_status = Fore.RED + str(x['ssl']) + Fore.RESET
-
-        t.add_row([node_name, url_link, port_no, ssl_status])
-
+    for node in nodes:
+      
+      node_name = Fore.GREEN + node['name'] + Fore.RESET
+      url_link = Fore.GREEN + node['url'] + Fore.RESET
+      port_no = Fore.YELLOW + str(node['port']) + Fore.RESET
+      
+      if node['ssl']:
+            ssl_status = Fore.GREEN + str(node['ssl']) + Fore.RESET
+      else:
+            ssl_status = Fore.RED + str(node['ssl']) + Fore.RESET
+     
+      t.add_row([node_name, url_link, port_no, ssl_status])
+    
     table = t.copy()
     t.clear_rows()
-    return {'table': table}
+   
+   return {'table': table}
